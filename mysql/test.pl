6a701eb06b0e8cc12af581da0e49825ca51c83d3
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-10-10 14:28:18 -0300

    dbi:mysql

---
+++ mysql/test.pl
@@ -0,0 +1,20 @@
+#!/usr/local/bin/perlbrew.sh
+
+use DBI;
+use Time::HiRes qw(time);
+$start = time;
+my $dbh = DBI->connect("dbi:mysql:host=127.0.0.1;dbi:mysql:database=mysql;port=3306",
+                     "root","mysql");
+my $user = 'root';
+my $sth = $dbh->prepare("SELECT Host FROM user WHERE User=?");
+$sth->execute($user);
+while (my $ref = $sth->fetchrow_hashref()) {
+  print "found a row: $ref->{'Host'}\n";
+}
+$sth->finish();
+my $rows = $sth->rows;
+print("total rows: $rows\n");
+undef $sth;
+$dbh->disconnect();
+undef $dbh;
+printf "%.6f\n", time() - $start;
