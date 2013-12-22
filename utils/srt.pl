276a76a1c9ec860304fa087e54d77a18566d676c
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2013-12-22 12:18:02 -0200

    qx

--- utils/srt.pl
+++ utils/srt.pl
@@ -9,24 +9,32 @@ sub trim {
     return $s 
 };
 
+sub readfile {
+    my ($enc, $fname) = @_;
+    if ($enc ne "utf-8") {
+        return qx|iconv -f $enc -t UTF-8//TRANSLIT $fname|;
+    }
+    open(my $fh_in, $fname);
+    my @subs = <$fh_in>;
+    close $fh_in;
+    return @subs;
+}
+
 if ($#ARGV < 0) {
     print "usage: srt.pl <file>\n";
     exit 1;
 }
 
-my $file = trim `file -bi $ARGV[0]`;
+my $file = trim qx|file -bi $ARGV[0]|;
 
 my @encoding = split "charset=", $file;
 
 exit(1) if @encoding == 1;
-exit(0) if $encoding[1] eq "utf-8";
 
-my @res = `iconv -f $encoding[1] -t UTF-8//TRANSLIT $ARGV[0]`;
+my @res = readfile($encoding[1], $ARGV[0]);
 
-if ($? == 0) {
-    open(my $fh_out, ">$ARGV[0]") or die $!;
-    my $srt = join '', @res;
-    $srt =~ s/<.*?i>|<.*?b>|<.*?u>//gi;
-    print $fh_out $srt;
-    close $fh_out;
-}
+open(my $fh_out, ">$ARGV[0]");
+my $srt = join '', @res;
+$srt =~ s/<.*?i>|<.*?b>|<.*?u>//gi;
+print $fh_out $srt;
+close $fh_out;
