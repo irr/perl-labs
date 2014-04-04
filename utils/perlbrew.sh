9ba31f5a3f84349a51bee62f70a91c4e4442f3c6
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-04-04 07:45:48 -0300

    opt

--- utils/perlbrew.sh
+++ utils/perlbrew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PERLBREW_ROOT=$HOME/perl5/perlbrew
+export PERLBREW_ROOT=/opt/perl5
 export PERLBREW_HOME=$HOME/.perlbrew
 source ${PERLBREW_ROOT}/etc/bashrc
 perlbrew use perl-5.14.4
