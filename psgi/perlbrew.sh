335c05082f3398320b67458e96c4ac12aa0dc96c
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-06-02 10:18:59 -0300

    perl-5.20.0

--- psgi/perlbrew.sh
+++ psgi/perlbrew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PERLBREW_ROOT=/opt/perl5
 source ${PERLBREW_ROOT}/etc/bashrc
-perlbrew use perl-5.14.4
+perlbrew use perl-5.20.0
 perl "$@"
--- utils/perlbrew.sh
+++ utils/perlbrew.sh
@@ -2,5 +2,5 @@
 export PERLBREW_ROOT=/opt/perl5
 export PERLBREW_HOME=$HOME/.perlbrew
 source ${PERLBREW_ROOT}/etc/bashrc
-perlbrew use perl-5.14.4
+perlbrew use perl-5.20.0
 perl "$@"
