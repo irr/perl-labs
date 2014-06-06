35db5b9e82893c3b087acc02138303a294445cdf
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-06-06 13:52:53 -0300

    async]

---
+++ lwp/mget.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+use 5.014;
+
+use threads;
+
+use LWP::UserAgent;
+use JSON;
+
+my $ua = LWP::UserAgent->new();
+my @threads;
+
+for my $url ('http://www.google.com/', 'http://www.perl.org/') {
+    push @threads, async { 
+        my $json = JSON->new->allow_nonref();
+        my %hash = (url => $url);
+        my $res = $ua->get($url);
+        $hash{size} = length($res->content);
+        $json->encode(\%hash);
+    };
+}
+
+for my $thread (@threads) {
+    my @data = $thread->join;
+    say @data;
+}
