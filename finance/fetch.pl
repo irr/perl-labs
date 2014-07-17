2ed2274d6b7da9c39e51c7db60d0a97c43960236
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-07-17 14:07:56 -0300

    math::business

--- finance/fetch.pl
+++ finance/fetch.pl
@@ -5,11 +5,17 @@ use DateTime;
 use Finance::Quote;
 use Finance::QuoteHist;
 
+use Math::Business::SMA;
+
 use PDL;
 use PDL::NiceSlice;
 use PDL::Finance::TA ':Func';
 
+use constant DAYS => 7;
+
 my $p = ($#ARGV < 0) ? "PETR4" : $ARGV[0];
+
+print "\nRetrieving quote (realtime)...\n";
 
 my $q = Finance::Quote->new("Yahoo::Brasil");
 $q->timeout(60);
@@ -21,17 +27,20 @@ foreach (qw(close open last date time exchange bid volume high low pe p_change n
     print "$_: $info{$p, $_}, ";
 }
 
+print "\n\nRetrieving quotes (historical)...\n";
+
 $q = Finance::QuoteHist->new(
     lineup => "Finance::QuoteHist::Google",
     symbols => [$p], 
     start_date => '1 month ago', 
     end_date => 'today',);
 
-print "\nquotes...\n";
+my @quotes = $q->quotes();
 
-my @quotes = ();
+my @pdlquotes = ();
+my @cells = ();
 
-foreach $row ($q->quotes()) { 
+foreach $row (@quotes) { 
     my ($sym, $date, $o, $h, $l, $c, $vol) = @$row;
     my ($yy, $mm, $dd) = split /\//, $date;
     my $epoch = DateTime->new(
@@ -41,12 +50,29 @@ foreach $row ($q->quotes()) {
                     hour => 16, minute => 0, second => 0,
                     time_zone => 'America/New_York',
                  )->epoch;    
-    push @quotes, pdl($epoch, $o, $h, $l, $c);
+    push @pdlquotes, pdl($epoch, $o, $h, $l, $c);
+    push @cells, $o;
     print "@$row\n"; 
 }
 
-my $data = pdl(@quotes)->transpose;
+print "\nPDL processing quotes (SMA)...\n";
+
+my $data = pdl(@pdlquotes)->transpose;
 
 print $data(0:-1,(1))."\n";
 
-print $data(0:-1,(1))->movavg(7)."\n";
+print "PDL(SMA): ".$data(0:-1,(1))->movavg(DAYS)."\n";
+
+print "\nMath::Business processing quotes (SMA)...\n";
+
+my @smas = ();
+my $limit = $#cells + 1 - DAYS;
+
+for (my $i = 0; $i <= $limit; $i++) {
+    my @d = @cells[$i..($i + DAYS - 1)];
+    my $avg = new Math::Business::SMA(DAYS);    
+    $avg->insert($_) for @d;
+    push @smas, $avg->query;
+}
+
+print "Math::Business(SMA): @smas\n";
