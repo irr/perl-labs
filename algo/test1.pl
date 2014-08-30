4287f967cb9221ff5271e08a7268790ec977da75
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2014-08-30 08:30:31 -0300

    fb2

--- algo/test1.pl
+++ algo/test1.pl
@@ -20,8 +20,23 @@ sub stickers {
     print "$w need $max sticker(s)\n";
 }
 
+sub stickers2 {
+    my $w = shift;
+    my $max = 0;
+    my %c;
+    foreach my $l (split //, $w) {
+        next if ($l eq " ");
+        $c{$l}++;
+        my $n = $c{$l} / $wc{$l};
+        $max = $n if ($n > $max);
+        print "$l: $c{$l} $wc{$l} $n\n";
+    }
+    print "$w need $max sticker(s)\n";
+}
 $wc{$_}++ for split //, "facebook";
 
 &stickers($_) for ("coffee kebab", "book", "ffacebook");
+print "=====================================\n";
+&stickers2($_) for ("coffee kebab", "book", "ffacebook");
 
 
