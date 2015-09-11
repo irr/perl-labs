2b95342992dc487f5a08963d02b6f3a9b07780ee
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-09-11 05:51:57 -0300

    timestamp

--- cassandra/rtseries.pl
+++ cassandra/rtseries.pl
@@ -1,23 +1,24 @@
 use DBI;
-use Time::HiRes qw( time );
-use Data::Dumper;
+use Modern::Perl;
 
 # CREATE KEYSPACE IF NOT EXISTS irr WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1 };
-# CREATE TABLE series (id text, ts text, val text, PRIMARY KEY (id,ts),) WITH CLUSTERING ORDER BY (ts DESC);
+# CREATE TABLE series (id text, ts timestamp, val text, PRIMARY KEY (id,ts),) WITH CLUSTERING ORDER BY (ts DESC);
 
 my $dbh= DBI->connect("dbi:Cassandra:keyspace=irr", undef, undef, {RaiseError => 1});
 
 my $ts = time();
 
 foreach my $i (0..9) {
-  $dbh->do("INSERT INTO series (id, ts, val) VALUES (?, ?, ?) USING TTL 20;",
-            undef, "irr-id", $ts--, "".$i);
+    say("inserting timestamp $i [$ts]...");
+    $dbh->do("INSERT INTO series (id, ts, val) VALUES (?, ?, ?) USING TTL 20;",
+             undef, "irr-id", $ts, "".$i);
+    $ts = $ts - 1000;
 }
  
 my $rows = $dbh->selectall_arrayref("SELECT * FROM series LIMIT 10;");
  
 for my $row (@$rows) {
-    print(Dumper($row));
+    say("retrieving timestamp ${$row}[1] = ${$row}[2]...");
 }
 
 $dbh->disconnect;
