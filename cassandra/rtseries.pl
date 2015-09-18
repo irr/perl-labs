b12e6be33fcb0eff9ce04b187d6ac2b37f47b57e
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-09-18 11:05:54 -0300

    timeuuid

--- cassandra/rtseries.pl
+++ cassandra/rtseries.pl
@@ -2,20 +2,17 @@ use DBI;
 use Modern::Perl;
 
 # CREATE KEYSPACE IF NOT EXISTS irr WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1 };
-# CREATE TABLE series (id text, ts timestamp, val text, PRIMARY KEY (id,ts),) WITH CLUSTERING ORDER BY (ts DESC);
+# CREATE TABLE series (id text, ts timeuuid, val int, PRIMARY KEY (id,ts),) WITH CLUSTERING ORDER BY (ts DESC);
 
 my $dbh= DBI->connect("dbi:Cassandra:keyspace=irr", undef, undef, {RaiseError => 1});
 
-my $ts = time();
-
 foreach my $i (0..9) {
-    say("inserting timestamp $i [$ts]...");
-    $dbh->do("INSERT INTO series (id, ts, val) VALUES (?, ?, ?) USING TTL 20;",
-             undef, "irr-id", $ts, "".$i);
-    $ts = $ts - 1000;
+    say("inserting timestamp $i...");
+    $dbh->do("INSERT INTO series (id, ts, val) VALUES (?, now(), ?) USING TTL 20;",
+             undef, "irr-id", $i);
 }
  
-my $rows = $dbh->selectall_arrayref("SELECT * FROM series LIMIT 10;");
+my $rows = $dbh->selectall_arrayref("SELECT id, unixTimestampOf(ts) as ts, val FROM series LIMIT 10;");
  
 for my $row (@$rows) {
     say("retrieving timestamp ${$row}[1] = ${$row}[2]...");
