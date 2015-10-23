7b5f20436c3e1d274379dcaa337395194a6cec91
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-10-23 13:49:56 -0200

    bucket

--- s3/nginx/auth.pm
+++ s3/nginx/auth.pm
@@ -10,9 +10,6 @@ use MIME::Base64;
 
 use Env qw(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY);
 
-my $BUCKET = "irrs3";
-my $HOST = "$BUCKET.s3.amazonaws.com";
-
 sub handler {
     my $r = shift;
 
@@ -26,12 +23,13 @@ sub handler {
 sub patch {
     my $r = shift;
 
+    my $bucket = $r->variable("bucket");
     my $key = (split('/', $r->uri))[1];
     my $dt = DateTime->now();
     my $now = $dt->strftime('%a, %d %b %Y %H:%M:%S GMT');
 
     my $hmac = Digest::HMAC_SHA1->new($AWS_SECRET_ACCESS_KEY);
-    $hmac->add("GET\n\n\n$now\n/$BUCKET/$key");
+    $hmac->add("GET\n\n\n$now\n/$bucket/$key");
     my $digest = MIME::Base64::encode_base64($hmac->digest, '');
 
     my $uri = sprintf("/proxy/%s:%s/%s%s", $AWS_ACCESS_KEY_ID, $digest, $now, $r->uri);
