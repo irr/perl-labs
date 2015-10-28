57fad569e84cd14f7bc14cfd801f0f5c010b87d3
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-10-28 14:04:40 -0200

    curl+ok

--- s3/nginx/auth.pm
+++ s3/nginx/auth.pm
@@ -13,8 +13,11 @@ use Env qw(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY);
 sub handler {
     my $r = shift;
 
-    if ($r->request_method ne "GET") {
-        return DECLINED;
+    if (($r->request_method ne "GET") and
+        ($r->request_method ne "PUT") and 
+        ($r->request_method ne "DELETE")) { 
+
+       return HTTP_BAD_REQUEST;
     }
 
     &patch($r);
@@ -23,15 +26,21 @@ sub handler {
 sub patch {
     my $r = shift;
 
+    my $method = $r->request_method;
     my $bucket = $r->variable("bucket");
     my $key = (split('/', $r->uri))[1];
     my $dt = DateTime->now();
     my $now = $dt->strftime('%a, %d %b %Y %H:%M:%S GMT');
-
     my $hmac = Digest::HMAC_SHA1->new($AWS_SECRET_ACCESS_KEY);
-    $hmac->add("GET\n\n\n$now\n/$bucket/$key");
-    my $digest = MIME::Base64::encode_base64($hmac->digest, '');
 
+    if ($method eq "PUT") {
+        my $ctype = $r->header_in("Content-Type");
+        $hmac->add("$method\n\n$ctype\n$now\n/$bucket/$key");
+    } else {
+        $hmac->add("$method\n\n\n$now\n/$bucket/$key");
+    }
+
+    my $digest = MIME::Base64::encode_base64($hmac->digest, '');
     my $uri = sprintf("/proxy/%s:%s/%s%s", $AWS_ACCESS_KEY_ID, $digest, $now, $r->uri);
     $r->internal_redirect($uri);
 
---
+++ s3/nginx/readme.txt
@@ -0,0 +1,20 @@
+curl -X PUT -T "${file}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  https://${bucket}.s3.amazonaws.com/${file}
+
+curl -v -H "Date: Mon, 26-Oct-15 15:37:43 GMT" -H "Authorization: AWS ...:...." http://irrs3.s3.amazonaws.com/test;echo
+curl -v -X PUT -T "s3.txt" -H "Date: Mon, 26-Oct-15 15:37:43 GMT" -H "Authorization: AWS ...:..." -H "Host: irrs3.s3.amazonaws.com" -H "Content-Type: text/plain" http://irrs3.s3.amazonaws.com/s3.txt;echo
+curl -v -X DELETE -H "Date: Mon, 26-Oct-15 15:37:43 GMT" -H "Authorization: AWS ...:..." -H "Host: irrs3.s3.amazonaws.com" http://irrs3.s3.amazonaws.com/s3.txt;echo
+
+curl -v http://localhost:8888/s3.txt
+curl -v http://localhost:8888/s3.jpg -o s3.jpg
+
+curl -v -X PUT --data "@s3.txt" http://localhost:8888/s3.txt
+curl -v -X PUT --data-binary "@s3.jpg" -H "Content-Type: image/jpeg" http://localhost:8888/s3.jpg
+
+curl -v X DELETE http://localhost:8888/s3.txt
+curl -v X DELETE http://localhost:8888/s3.jpg
+
